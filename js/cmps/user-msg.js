c1167b26de701d9124d91b9ce969c74794917cc2
Tal Ben David <talbd51@gmail.com>
2021-06-25 11:40:16 +0300

    animation on user msg

--- js/cmps/user-msg.js
+++ js/cmps/user-msg.js
@@ -2,17 +2,30 @@ import { eventBus } from '../services/event-bus-service.js';
 
 export default {
     template: `
-        <div v-if="msg" class="user-msg" :class="msg.type">
+        <div 
+        v-if="msg" 
+        class="user-msg" 
+        :class="msg.type"
+        enter-active-class="animate__animated animate__bounceInDown"
+        enter-active-class="animate__animated animate__bounceInDown"
+        
+        >
             <p>{{msg.txt}}</p>
         </div>
     `,
     data() {
         return {
-            msg: null
+            msg: null,
+            animation: null
         };
     },
     created() {
         eventBus.$on('show-msg', this.showMsg);
+    },
+    computed: {
+        isAnimation() {
+            if (this.msg) return 'animate__animated animate__bounceInDown'
+        }
     },
     destroyed() {
         eventBus.$off('show-msg', this.showMsg);
