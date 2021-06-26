aee0133808144b7b3177fdbb44fb2e20fe61c4b3
Guy Geva <guy1030@gmail.com>
2021-06-26 23:38:23 +0300

    Final commit before presentation - color pallete img src fixed for Github pages

--- js/apps/keep/cmps/color-palette.js
+++ js/apps/keep/cmps/color-palette.js
@@ -5,15 +5,15 @@ export default {
     props: ['note'],
     template: `
     <div class="color-palette">
-        <img @click="selectColor" id="orange" class="color"src="../img/colors/orange.png">
-        <img @click="selectColor" id="lightseagreen" class="color"src="../img/colors/aqua.png">
-        <img @click="selectColor" id="rgb(196, 59, 81)" class="color"src="../img/colors/pink.png">
-        <img @click="selectColor" id="gold" class="color"src="../img/colors/yellow.png"> 
-        <img @click="selectColor" id="darkred" class="color "src="../img/colors/red.png">
-        <img @click="selectColor" id="rgb(192, 78, 192)" class="color" src="../img/colors/violet.png">
-        <img @click="selectColor" id="lightslategray" class="color"src="../img/colors/gray.png">
-        <img @click="selectColor" id="#3e3e3e" class="color"src="../img/colors/black.png">
-        <img @click="selectColor" id="whitesmoke" class="color"src="../img/colors/white.png">
+        <img @click="selectColor" id="orange" class="color"src="img/colors/orange.png">
+        <img @click="selectColor" id="lightseagreen" class="color"src="img/colors/aqua.png">
+        <img @click="selectColor" id="rgb(196, 59, 81)" class="color"src="img/colors/pink.png">
+        <img @click="selectColor" id="gold" class="color"src="img/colors/yellow.png"> 
+        <img @click="selectColor" id="darkred" class="color "src="img/colors/red.png">
+        <img @click="selectColor" id="rgb(192, 78, 192)" class="color" src="img/colors/violet.png">
+        <img @click="selectColor" id="lightslategray" class="color"src="img/colors/gray.png">
+        <img @click="selectColor" id="#3e3e3e" class="color"src="img/colors/black.png">
+        <img @click="selectColor" id="whitesmoke" class="color"src="img/colors/white.png">
     </div>
     `,
     data() {
